f175d486a63af82f626a03fc83917858668f7adb
mitchellspink <61097082+mitchellspink@users.noreply.github.com>
2020-12-16 04:07:07 +0000

    Add files via upload

---
+++ javaFudamentals/firstJava/MitchellSpink.java
@@ -0,0 +1,7 @@
+public class MitchellSpink {
+    public static void main(String[] args) {
+        System.out.println("My name is Mitchell Spink");
+        System.out.println("I am 23 years old");
+        System.out.println("My hometown is Brea, Ca");
+    }
+}
---
+++ javaFudamentals/fizzBuzz/FizzBuzz.java
@@ -0,0 +1,15 @@
+// Write a program that will take an integer and print Fizz if the number is divisible by 3, Buzz if the number is divisible by 5, FizzBuzz if the number is divisible by 3 and 5, and the number itself for all other cases.
+
+public class FizzBuzz {
+    public String fizzBuzz (int number) {
+        if (number % 3==0 && number % 5==0) {
+            return "FizzBuzz";
+        } else if(number % 3 == 0) {
+            return "Fizz";
+        } else if(number % 5 == 0) {
+            return "Buzz";
+        } else {
+            return String.valueOf(number);
+        }
+    }
+}
